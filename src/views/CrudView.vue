25a34ca6650f55e43b0d931df5e71e532443afd9
AntonioBorges20 <20393128@utcancun.edu.mx>
2024-02-17 16:40:39 -0500

    Ultimate commit

--- src/views/CrudView.vue
+++ src/views/CrudView.vue
@@ -5,14 +5,13 @@
         <tr>
           <th>Name</th>
           <th>Email</th>
-          <!-- Otros campos -->
         </tr>
       </thead>
       <tbody>
         <tr v-for="user in users" :key="user.id">
           <td>{{ user.name }}</td>
           <td>{{ user.email }}</td>
-          <!-- Otros campos -->
+
         </tr>
       </tbody>
     </table>
@@ -23,8 +22,11 @@
 import { ref } from 'vue';
 
 const users = ref([
-  { id: 1, name: 'John Doe', email: 'john@example.com' },
-  { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
-  // Agrega más usuarios genéricos aquí
+  { id: 1, name: 'Antonio', email: 'antonio@correo.com' },
+  { id: 2, name: 'Misael', email: 'misael@correo.com' },
+  { id: 3, name: 'Osmar', email: 'osmar@correo.com' },
+  { id: 4, name: 'Jair', email: 'jair@correo.com' },
+  { id: 5, name: 'Julio', email: 'julio@correo.com' },
+  { id: 6, name: 'Pablo', email: 'pablo@correo.com' },
 ]);
 </script>
