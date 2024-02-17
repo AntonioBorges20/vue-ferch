9848f325b3c53e2961091adddcab50bd5886db29
AntonioBorges20 <20393128@utcancun.edu.mx>
2024-02-17 12:53:42 -0500

    first commit

---
+++ src/views/CrudView.vue
@@ -0,0 +1,12 @@
+<template>
+    <main>
+      <h1>crud</h1>
+      <p>Welcome to the crud page</p>
+    </main>
+  </template>
+  
+  <script setup lang="ts">
+ 
+  
+  </script>
+  
---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <form action="">
+      <h1>Login</h1>
+      <input type="email" placeholder="Email Address" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+const 
+
+</script>
